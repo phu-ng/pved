d64aae3974be5339463f02c9e076ae976203f62e
Phu Nguyen <phunguyen9297@gmail.com>
2024-03-26 16:00:42 +0700

    use mockito for better test experience

--- src/main.rs
+++ src/main.rs
@@ -150,43 +150,123 @@ fn is_public_ipv6(ipv6: &String) -> bool {
 
 #[cfg(test)]
 mod tests {
-    use std::collections::HashMap;
+    use crate::models::{IpAddress, NetworkInterface, NetworkInterfaceResult, Node, Qemu};
     use super::*;
 
     #[tokio::test]
-    async fn test_get_nodes() {
-        let base_url = env::var("BASE_URL").unwrap().as_str();
-        let api_key = env::var("API_KEY").unwrap().as_str();
+    async fn test_get_nodes_ok() {
+        // Request a new server from the pool
+        let mut server = mockito::Server::new_async().await;
 
-        let nodes = get_nodes(base_url, api_key).await.unwrap();
-        assert_eq!(nodes, vec!["x300", "k4"])
+        // Use one of these addresses to configure your client
+        let url = server.url();
+        let api_key = "1234";
+
+        // Create a mock
+        let node1 = Node::new("online".to_string(), "node/k4".to_string(), "k4".to_string(), "node".to_string());
+        let node2 = Node::new("online".to_string(), "node/x300".to_string(), "x300".to_string(), "node".to_string());
+        let nodes = vec![node1, node2];
+        let node_data = NodeData::new(nodes);
+
+        server.mock("GET", "/nodes")
+            .with_status(201)
+            .with_header("content-type", "application/json")
+            .with_header("authorization", "1234")
+            .with_body(serde_json::to_string(&node_data).unwrap())
+            .create();
+
+        let nodes = get_nodes(url.as_str(), api_key).await.unwrap();
+        assert_eq!(nodes, vec!["k4", "x300"])
     }
 
     #[tokio::test]
     async fn test_get_qemus() {
-        let base_url = env::var("BASE_URL").unwrap().as_str();
-        let api_key = env::var("API_KEY").unwrap().as_str();
-        let nodes = vec!["x300".to_string()];
+        // Request a new server from the pool
+        let mut server = mockito::Server::new_async().await;
 
-        let qemus = get_qemus(base_url, api_key, nodes).await.unwrap();
+        // Use one of these addresses to configure your client
+        let url = server.url();
+        let api_key = "1234";
 
-        let mut result = HashMap::new();
-        result.entry("x300".to_string()).or_insert(vec![201, 204]);
-        assert_eq!(qemus, result)
+        let nodes = vec!["node1".to_string(), "node2".to_string()];
+        let node1_qemu_100 = Qemu::new("running".to_string(), "vm1".to_string(), Some("container;k8s".to_string()), 100);
+        let node1_qemu_101 = Qemu::new("running".to_string(), "vm2".to_string(), Some("watch;database".to_string()), 101);
+        let node2_qemu_200 = Qemu::new("running".to_string(), "vm3".to_string(), Some("k8s;watch".to_string()), 200);
+        let node2_qemu_201 = Qemu::new("running".to_string(), "vm4".to_string(), Some("watch;iot".to_string()), 201);
+        let node1_qemus = QemuData::new(vec![node1_qemu_100, node1_qemu_101]);
+        let node2_qemus = QemuData::new(vec![node2_qemu_200, node2_qemu_201]);
+
+        server.mock("GET", "/nodes/node1/qemu")
+            .with_status(201)
+            .with_header("content-type", "application/json")
+            .with_header("authorization", "1234")
+            .with_body(serde_json::to_string(&node1_qemus).unwrap())
+            .create();
+
+        server.mock("GET", "/nodes/node2/qemu")
+            .with_status(201)
+            .with_header("content-type", "application/json")
+            .with_header("authorization", "1234")
+            .with_body(serde_json::to_string(&node2_qemus).unwrap())
+            .create();
+
+        let mut expected = HashMap::new();
+        expected.entry("node1".to_string()).or_insert(vec![101]);
+        expected.entry("node2".to_string()).or_insert(vec![200, 201]);
+
+        let qemu_ids = get_qemus(url.as_str(), api_key, nodes).await.unwrap();
+        assert_eq!(qemu_ids, expected)
     }
 
     #[tokio::test]
     async fn test_get_ips() {
-        let base_url = env::var("BASE_URL").unwrap().as_str();
-        let api_key = env::var("API_KEY").unwrap().as_str();
+        // Request a new server from the pool
+        let mut server = mockito::Server::new_async().await;
 
-        let vm_ids = vec![201, 204];
-        let mut vms = HashMap::new();
-        vms.entry("x300".to_string()).or_insert(vm_ids);
+        // Use one of these addresses to configure your client
+        let url = server.url();
+        let api_key = "1234";
 
-        let ips = get_ips(base_url, api_key, vms).await.unwrap();
+        let mut qemu_ids = HashMap::new();
+        qemu_ids.entry("node1".to_string()).or_insert(vec![101, 102]);
 
-        assert_eq!(ips, vec!["2405:4803:fe85:1e0:be24:11ff:fe51:9939".to_string()])
+        let qemu_101_net_interface_eth0 = NetworkInterface::new("00:00:00:00:00:00".to_string(), "eth0".to_string(),
+                                                                vec![IpAddress::new("ipv4".to_string(), "192.168.1.34".to_string(), 24),
+                                                                     IpAddress::new("ipv6".to_string(), "fdbc:81e7:7a9a::".to_string(), 48)]);
+        let qemu_101_net_interface_lo = NetworkInterface::new("00:00:00:00:00:00".to_string(), "lo".to_string(),
+                                                              vec![IpAddress::new("ipv4".to_string(), "127.0.0.1".to_string(), 8),
+                                                                   IpAddress::new("ipv6".to_string(), "::1".to_string(), 128)]);
+        let qemu_101_net_interface_docker = NetworkInterface::new("00:00:00:00:00:00".to_string(), "docker0".to_string(),
+                                                                  vec![IpAddress::new("ipv4".to_string(), "172.16.0.1".to_string(), 16)]);
+
+        let qemu_102_net_interface_ens18 = NetworkInterface::new("00:00:00:00:00:00".to_string(), "ens18".to_string(),
+                                                                 vec![IpAddress::new("ipv4".to_string(), "192.168.1.35".to_string(), 24),
+                                                                      IpAddress::new("ipv6".to_string(), "2404:6800:4005:815::200e".to_string(), 64),
+                                                                      IpAddress::new("ipv6".to_string(), "2504:6800:4005:815::200d".to_string(), 64)]);
+        let qemu_102_net_interface_lo = qemu_101_net_interface_lo.clone();
+        let qemu_102_net_interface_docker = qemu_101_net_interface_docker.clone();
+
+        let qemu_101_net_interface_data = NetworkInterfaceData::new(
+            NetworkInterfaceResult::new(vec![qemu_101_net_interface_docker, qemu_101_net_interface_eth0, qemu_101_net_interface_lo]));
+        let qemu_102_net_interface_data = NetworkInterfaceData::new(
+            NetworkInterfaceResult::new(vec![qemu_102_net_interface_docker, qemu_102_net_interface_lo, qemu_102_net_interface_ens18]));
+
+        server.mock("GET", "/nodes/node1/qemu/101/agent/network-get-interfaces")
+            .with_status(201)
+            .with_header("content-type", "application/json")
+            .with_header("authorization", "1234")
+            .with_body(serde_json::to_string(&qemu_101_net_interface_data).unwrap())
+            .create();
+
+        server.mock("GET", "/nodes/node1/qemu/102/agent/network-get-interfaces")
+            .with_status(201)
+            .with_header("content-type", "application/json")
+            .with_header("authorization", "1234")
+            .with_body(serde_json::to_string(&qemu_102_net_interface_data).unwrap())
+            .create();
+
+        let ips = get_ips(url.as_str(), api_key, qemu_ids).await.unwrap();
+        assert_eq!(ips, vec!["2404:6800:4005:815::200e".to_string()])
     }
 }
 
--- src/models.rs
+++ src/models.rs
@@ -1,57 +1,79 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Node {
     #[allow(dead_code)]
     status: String,
     id: String,
-    ssl_fingerprint: String,
     pub(crate) node: String,
     r#type: String,
-    level: String,
 }
 
-#[derive(Debug, Deserialize)]
+impl Node {
+    pub fn new(status: String, id: String, node: String, r#type: String) -> Self {
+        Self { status, id, node, r#type }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
 pub struct NodeData {
     pub(crate) data: Vec<Node>,
 }
 
-#[derive(Debug, Deserialize)]
+impl NodeData {
+    pub fn new(data: Vec<Node>) -> Self {
+        Self { data }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Qemu {
-    netin: u64,
-    cpus: u32,
-    netout: u64,
     status: String,
     name: String,
-    maxmem: u64,
-    disk: u64,
-    uptime: u64,
     pub(crate) tags: Option<String>,
-    pid: Option<u32>,
-    cpu: f64,
-    diskwrite: u64,
-    diskread: u64,
-    maxdisk: u64,
-    mem: u64,
     pub(crate) vmid: u32,
 }
 
-#[derive(Debug, Deserialize)]
+impl Qemu {
+    pub fn new(status: String, name: String, tags: Option<String>, vmid: u32) -> Self {
+        Self { status, name, tags, vmid }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
 pub struct QemuData {
     pub(crate) data: Vec<Qemu>,
 }
 
-#[derive(Debug, Deserialize)]
+impl QemuData {
+    pub fn new(data: Vec<Qemu>) -> Self {
+        Self { data }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
 pub struct NetworkInterfaceData {
-    pub(crate) data: NetworkInterfaceResult
+    pub(crate) data: NetworkInterfaceResult,
 }
 
-#[derive(Debug, Deserialize)]
+impl NetworkInterfaceData {
+    pub fn new(data: NetworkInterfaceResult) -> Self {
+        Self { data }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
 pub struct NetworkInterfaceResult {
-    pub(crate) result: Vec<NetworkInterface>
+    pub(crate) result: Vec<NetworkInterface>,
 }
 
-#[derive(Debug, Deserialize)]
+impl NetworkInterfaceResult {
+    pub fn new(result: Vec<NetworkInterface>) -> Self {
+        Self { result }
+    }
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct NetworkInterface {
     #[serde(rename = "hardware-address")]
     hardware_address: String,
@@ -60,13 +82,25 @@ pub struct NetworkInterface {
     pub(crate) ip_addresses: Vec<IpAddress>,
 }
 
-#[derive(Debug, Deserialize)]
+impl NetworkInterface {
+    pub fn new(hardware_address: String, name: String, ip_addresses: Vec<IpAddress>) -> Self {
+        Self { hardware_address, name, ip_addresses }
+    }
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct IpAddress {
     #[serde(rename = "ip-address-type")]
     pub(crate) ip_address_type: String,
     #[serde(rename = "ip-address")]
     pub(crate) ip_address: String,
     prefix: u32,
+}
+
+impl IpAddress {
+    pub fn new(ip_address_type: String, ip_address: String, prefix: u32) -> Self {
+        Self { ip_address_type, ip_address, prefix }
+    }
 }
 
 #[derive(Debug, Serialize)]
