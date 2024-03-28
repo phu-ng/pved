791818adae48296dfd991e1d8321b3fa53f5791b
Phu Nguyen <phunguyen9297@gmail.com>
2024-03-28 11:59:58 +0700

    update ipv6 checking function

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -3,6 +3,7 @@ pub fn is_public_ipv6(ipv6: &str) -> bool {
         "fd",
         "fc",
         "fe80",
+        "::",
     ];
 
     for prefix in private_prefixes {
@@ -12,4 +13,22 @@ pub fn is_public_ipv6(ipv6: &str) -> bool {
     }
 
     return true;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_check_ipv6() {
+        let ipv6_addr = "2001:0db8:85a3:0000:0000:8a2e:0370:7334"; // Global IPv6 address
+        let ipv6_addr2 = "fe80::c8e1:31ff:fe9f:bc77"; // Link-local IPv6 address
+        let ipv6_addr3 = "::1"; // Loopback IPv6 address
+        let ipv6_addr4 = "fc01:abcd::1";
+
+        assert_eq!(is_public_ipv6(ipv6_addr), true);
+        assert_eq!(is_public_ipv6(ipv6_addr2), false);
+        assert_eq!(is_public_ipv6(ipv6_addr3), false);
+        assert_eq!(is_public_ipv6(ipv6_addr4), false);
+    }
 }
