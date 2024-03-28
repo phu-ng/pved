bac973547de961395d78137f8b069d6226d3a596
Phu Nguyen <phunguyen9297@gmail.com>
2024-03-28 15:40:16 +0700

    listen on all interfaces - including ipv6

--- src/main.rs
+++ src/main.rs
@@ -40,7 +40,7 @@ async fn main() -> std::io::Result<()> {
             .service(discover)
     })
         .workers(2)
-        .bind(("0.0.0.0", 8080))?
+        .bind("[::]:8080")?
         .run()
         .await
 }
